6bd2ca98075b15756ef08c91254c20fcb6d0b51c
loic <loic.collatuzo@ynov.com>
2025-03-03 10:27:16 +0100

    go init

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	
+	"fmt"
+	"html/template"
+	"net/http"
 
-var weekday string
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	var fileName = "./frontend/template/home/index.html"
+	t, err := template.ParseFiles(fileName)
+	if err != nil {
+		fmt.Println("Erreur pendant le parsing", err)
+		return
+	}
+
+	t.Execute(w, nil)
+}
 
 func main() {
-	fmt.Printf("Today is %s", weekday)
+	fs := http.FileServer(http.Dir("../assets/"))
+	http.HandleFunc("/", home)
+	http.Handle("frontend/public/css", http.StripPrefix("frontend/public/css", fs))
+	http.Handle("frontend/public/js", http.StripPrefix("frontend/public/js", fs))
+	http.ListenAndServe("", nil)
 }
