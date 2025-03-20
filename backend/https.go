8b87da322251bd3dfff02c468b43ab0041f39c57
NMQ34 <theo.deschler@ynov.comm>
2025-03-20 10:15:23 +0100

    https-all-pages

--- backend/https.go
+++ backend/https.go
@@ -1,15 +1,15 @@
 package backend
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 )
 
-func StartSecureServer(handler http.Handler) {
-	server := &http.Server{
-		Addr:    ":443",
-		Handler: handler,
-	}
-
-	log.Fatal(server.ListenAndServeTLS("localhost+2.pem", "localhost+2-key.pem"))
+func StartSecureServer(handler http.Handler) {    
+    server := &http.Server{
+        Addr:    ":443",
+        Handler: handler,
+    }
+    
+    log.Fatal(server.ListenAndServeTLS("localhost+2.pem", "localhost+2-key.pem"))
 }
